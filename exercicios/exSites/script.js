efa8e11867ea1512ec8879dc5660bee906573475
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2023-07-22 21:24:11 -0300

    Site para horas

--- exercicios/exSites/script.js
+++ exercicios/exSites/script.js
@@ -1,10 +1,17 @@
-var agora = new Date()
-var hora = agora.getHours
-
-if (hora < 12){
-    document.
-} else if (){
-
-} else {
-    
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var agora = new Date()
+    var hora = agora.getHours()
+    msg.innerText = `Agora são exatamente ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        document.body.style.background = '#e2cd9f'
+        img.src = "manha.png"
+    } else if (hora <=18){
+        document.body.style.background = '#b9846f'
+        img.src = "tarde.png"
+    } else {
+        document.body.style.background = '#515154'
+        img.src = "noite.png"
+    }
 }
